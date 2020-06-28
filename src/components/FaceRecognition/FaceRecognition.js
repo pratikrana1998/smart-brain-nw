c97fa144000b5d2be76c8de45e685e57cfb67332
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-06-28 13:26:39 +0530

    adding multiple face detection using arrays

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './FaceRecognition.css'
 
-const FaceRecognition = ({imageURL, box}) => {
+const FaceRecognition = ({imageURL, boxes}) => {
   return (
   // Div to display image
   <div className='center ma'>
     <div className='absolute mt2'>
       <img id='inputImage' alt='' src={imageURL} width='500px' height='auto'/>
-      <div className='bounding-box' style={{
-          top: box.topRow,
-          right: box.rightCol,
-          bottom: box.bottomRow,
-          left: box.leftCol
-        }}></div>
+      {
+        boxes.map(box => {
+          return <div key={box.topRow} className='bounding-box' style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}></div>
+        })
+      }
     </div>
   </div>);
 }
